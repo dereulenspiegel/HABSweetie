9496d64d6505be3b3ab47c59551bc824b79fd7bc
Till Klocke <till.klocke@gmail.com>
2014-01-08 14:25:38 +0100

    Added first robolectric test, but it has to be ignored for now

--- App/HABSweetie/src/test/java/de/akuz/android/openhab/ui/PageActivityTest.java
+++ App/HABSweetie/src/test/java/de/akuz/android/openhab/ui/PageActivityTest.java
@@ -1,16 +1,21 @@
 package de.akuz.android.openhab.ui;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(manifest = "AndroidManifest.xml", qualifiers = "de-xhdpi", reportSdk = 10)
 public class PageActivityTest {
 
 	@Test
+	@Ignore
 	public void testPageActivityStart() throws Exception {
+		// Ignored until problems with ABS and robolectric are solved
 		PageActivity activity = Robolectric.buildActivity(PageActivity.class)
 				.create().get();
 		Assert.assertNotNull(activity.getObjectGraph());
